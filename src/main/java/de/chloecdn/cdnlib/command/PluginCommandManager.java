b445251e793559f675cbfaa970a2bbe279a8a9f0
BuildTools <unconfigured@null.spigotmc.org>
2022-08-13 00:07:39 +0200

    v1.6: - Removed TabCompleter, added method to register commands automatically. - Fixed Translated Messages stripping messages when sent to players.

--- src/main/java/de/chloecdn/cdnlib/command/PluginCommandManager.java
+++ src/main/java/de/chloecdn/cdnlib/command/PluginCommandManager.java
@@ -5,21 +5,20 @@ import de.chloecdn.cdnlib.util.Logging;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
-
-import java.util.List;
 
 public class PluginCommandManager {
 
     /**
-     * Registers the given command, that will run the given runAction when someone runs it, and also using the given tabCompleteAction.
+     * Registers the given command, that will run the given runAction when someone runs it.
      * NOTE: You still need to register this command in your "plugin.yml", or else it'll log an error and won't register that command.
      *
-     * @param command           The name of the command to register.
-     * @param runAction         the action to perform when someone runs the command.
-     * @param tabCompleteAction a list of Strings used for the TabCompletion, similar to Bukkit's own implementation.
+     * @param command   The name of the command to register.
+     * @param runAction the action to perform when someone runs the command.
+     * @deprecated This method is deprecated in favor of the method {@link PluginCommandManager#registerVanilla(String, RunAction)} (String, RunAction)},
+     * which doesn't require adding every command to the 'plugin.yml' manually.
      */
-    public static void register(@NotNull String command, @NotNull RunAction runAction, @Nullable TabCompleteAction tabCompleteAction) {
+    @Deprecated
+    public static void register(@NotNull String command, @NotNull RunAction runAction) {
         if (CDNLib.getPlugin().getCommand(command) == null) {
             Logging.error("Error while registering command '" + command + "'. Did you forget to register it in your 'plugin.yml'?");
             return;
@@ -28,17 +27,24 @@ public class PluginCommandManager {
             runAction.run(sender, cmd, label, args);
             return false;
         });
-        if (tabCompleteAction == null) {
-            return;
-        }
-        CDNLib.getPlugin().getCommand(command).setTabCompleter(tabCompleteAction::tabComplete);
+    }
+
+    /**
+     * Registers the given command, that will run the given runAction when someone runs it.
+     * Using this method doesn't require you to add it to the plugin.yml. Instead it uses Reflection to "inject" the command into vanilla's CommandMap
+     *
+     * @param command           The name of the command to register.
+     * @param runAction         the action to perform when someone runs the command.
+     */
+    public static void registerVanilla(@NotNull String command, @NotNull RunAction runAction) {
+        CommandUtils.addCommandToMap(command);
+        CDNLib.getPlugin().getCommand(command).setExecutor((sender, cmd, label, args) -> {
+            runAction.run(sender, cmd, label, args);
+            return false;
+        });
     }
 
     public interface RunAction {
         void run(CommandSender sender, Command command, String label, String[] args);
-    }
-
-    public interface TabCompleteAction {
-        List<String> tabComplete(CommandSender sender, Command command, String label, String[] args);
     }
 }
--- src/main/java/de/chloecdn/cdnlib/io/Translation.java
+++ src/main/java/de/chloecdn/cdnlib/io/Translation.java
@@ -2,6 +2,10 @@ package de.chloecdn.cdnlib.io;
 
 import de.chloecdn.cdnlib.CDNLib;
 import de.chloecdn.cdnlib.util.KeyValuePair;
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Translation {
@@ -11,16 +15,25 @@ public class Translation {
     /**
      * This can be used to translate messages into different languages, or generally make messages configurable.
      * In the config.yml, they'll always be taken from the path "translations.yourEntry"
-     * @param entry the entry name of your translation.
+     *
+     * @param entry        the entry name of your translation.
      * @param replacements the replacements you want to use. Useful if in your config you use messages like "{player} joined the game"
      * @return the translated String, or the given entry if none found.
      */
     @SafeVarargs
-    public static String getTranslation(String entry, KeyValuePair<String, String>... replacements) {
+    public static String getMessageTranslation(String entry, KeyValuePair<String, String>... replacements) {
         String s = plugin.getConfig().getString("translations." + entry, entry);
         for (KeyValuePair<String, String> replacement : replacements) {
             s = s.replace(replacement.key(), replacement.value());
         }
         return s.replace("&", "§");
+    }
+
+    public static void sendMessage(CommandSender sender, String message) {
+        sender.sendMessage(sender instanceof Player ? message : ChatColor.stripColor(message));
+    }
+
+    public static void broadcastMessage(String message) {
+        Bukkit.broadcastMessage(message);
     }
 }
