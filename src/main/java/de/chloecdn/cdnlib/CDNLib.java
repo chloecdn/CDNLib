c8692b686b7091964fad986bc766fc8771aad4bc
BuildTools <unconfigured@null.spigotmc.org>
2022-08-13 05:32:13 +0200

    v1.6.1 - Added Logging-Message on initialization

--- src/main/java/de/chloecdn/cdnlib/CDNLib.java
+++ src/main/java/de/chloecdn/cdnlib/CDNLib.java
@@ -1,5 +1,6 @@
 package de.chloecdn.cdnlib;
 
+import de.chloecdn.cdnlib.util.Logging;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class CDNLib {
@@ -11,6 +12,7 @@ public final class CDNLib {
 
     public CDNLib(JavaPlugin plugin) {
         CDNLib.plugin = plugin;
+        Logging.info("Initialized CDNLib.");
     }
 
     public static JavaPlugin getPlugin() {
